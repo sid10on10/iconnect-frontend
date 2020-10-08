8aa4ceef39f3c1ead64115597aaa15a7c4361b58
sid10on10 <siddhantk.singh.phe14@itbhu.ac.in>
2020-10-08 22:56:42 +0530

    first commit

---
+++ src/Components/signup.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+            </form>
+        );
+    }
+}
